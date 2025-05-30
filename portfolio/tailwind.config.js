935f43dcc7901a00a0efcf20f8746fb9693e9343
Siya Jariwala <siya.jari14@gmail.com>
2025-05-30 11:33:26 -0700

    Initial commit - Portfolio setup

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans':['inter','sans-serif']
+      }
+    },
+  },
+  plugins: [tailwindScrollbar],
+}
