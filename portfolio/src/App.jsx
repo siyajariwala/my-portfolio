85c53133d3c76bb10fd4bb161af0d53efa415919
Siya Jariwala <siya.jari14@gmail.com>
2025-06-02 10:53:24 -0700

    Completed the header section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,10 @@
+import Header from "./components/Header";
+
+
 const App =()=>{
-    return <h1>Hello</h1>
+  return(
+<Header/>
+  )
 }
 
 export default App;
---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Navbar from "./Navbar";
+const Header = () => {
+  const [navOpen,setNavOpen]=useState(false)
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+
+      <h1>
+        <a href="/" className="logo"></a>
+        <img src="public/images/logo.svg" width={40} height={40} alt="Siya Jariwala"
+/>      </h1>
+<div className="relative md:justify-self-center">
+  <button className="menu-btn sm:block md:hidden "
+   onClick={()=>setNavOpen((prev)=>!prev)}>
+    <span className="material-symbols-rounded">{navOpen ? 'close':'menu' }</span>
+  </button>
+  <Navbar navOpen={navOpen}/>
+</div>
+
+<a href="#contact"
+ className="btn btn-secondary max-md:hidden md:justify-self-end">Contact me</a>
+      </div>
+    </header>
+    
+  );
+}
+
+export default Header;
