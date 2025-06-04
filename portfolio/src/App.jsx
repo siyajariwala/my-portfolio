b648a7c7bbfd0260f0d72568b80cb781030d22ec
Siya Jariwala <siya.jari14@gmail.com>
2025-06-04 10:41:49 -0700

    Completed the hero section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
-
+import Hero from "./components/Hero";
 
 const App =()=>{
   return(
-<Header/>
+    <>
+    <Header/>
+<main>
+  <Hero/>
+</main>
+    </>
+
   )
 }
 
