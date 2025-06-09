ebaf06c6b90d541085e7d3b198f7a0eac5a7f4c4
Siya Jariwala <siya.jari14@gmail.com>
2025-06-09 10:00:52 -0700

    Completed the work section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 const App =()=>{
   return(
@@ -11,6 +12,7 @@ const App =()=>{
   <Hero/>
   <About/>
   <Skill/>
+  <Work/>
 </main>
     </>
 
