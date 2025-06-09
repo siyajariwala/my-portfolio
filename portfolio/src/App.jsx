6eb37aa5d903129f25051b651f019644aba1de2b
Siya Jariwala <siya.jari14@gmail.com>
2025-06-09 09:14:28 -0700

    Completed the skills section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skill from "./components/Skill";
 
 const App =()=>{
   return(
@@ -9,6 +10,7 @@ const App =()=>{
 <main>
   <Hero/>
   <About/>
+  <Skill/>
 </main>
     </>
 
