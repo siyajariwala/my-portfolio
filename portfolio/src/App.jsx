7dae94189b016352c6e2195e9e35a0e1c6ff573b
Siya Jariwala <siya.jari14@gmail.com>
2025-06-10 13:40:32 -0700

    Completed the review section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
+import Review from "./components/Review";
 
 const App =()=>{
   return(
@@ -13,6 +14,7 @@ const App =()=>{
   <About/>
   <Skill/>
   <Work/>
+  <Review/>
 </main>
     </>
 
